6b7666f817837726f989429712e9412f97390602
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-23 16:56:02 -0700

    feat:implement infra

--- src/components/ui/Skills/Infrastructure.tsx
+++ src/components/ui/Skills/Infrastructure.tsx
@@ -2,6 +2,16 @@ export const Infrastructure = () => {
   return (
     <>
       <p>this is infrastructure</p>
+      <div className="flex flex-wrap">
+        <img src="/infra-skillset/bun.svg" />
+        <img src="/infra-skillset/git.svg" />
+        <img src="/infra-skillset/Jest.svg" />
+        <img src="/infra-skillset/MongoDB.svg" />
+        <img src="/infra-skillset/mongoose.svg" />
+        <img src="/infra-skillset/neon.svg" />
+        <img src="/infra-skillset/supabase.svg" />
+        <img src="/infra-skillset/vite.svg" />
+      </div>
     </>
   );
 };
