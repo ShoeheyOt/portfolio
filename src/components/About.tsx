504baeef9b22ed49616f7ef99a555f6cd2f3ff23
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-23 01:34:30 -0700

    implement about part

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,21 @@
 export const About = () => {
   return (
-    <div id="about" className="w-1/2 border border-white">
-      <div>this is about part</div>
+    <div id="about" className="w-1/2 mx-4">
+      <p className="mt-8 mb-4">
+        Thank you for finding me! <br />
+        As enthusiastic full stack developer based in Vancouver, Canada, I am
+        passionate about developing user-centered application. currently
+        focusing on
+        <span className="text-danger"> Hono</span>,{" "}
+        <span className="text-danger">react</span> and{" "}
+        <span className="text-danger">postgreSQL</span>.
+        <br />
+      </p>
+      <p>
+        When I am not coding, I enjoy hiking, playing basketball and drinking
+        beer with my friends. also love to explore a brewery, for now, the best
+        is Philips Brewing in Victoria.
+      </p>
     </div>
   );
 };
