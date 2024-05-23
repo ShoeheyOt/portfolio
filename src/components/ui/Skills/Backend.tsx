7c4dd68dbd83fe042e457caae5ea16689cb1714c
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-23 16:37:50 -0700

    implement:add backend skill set

--- src/components/ui/Skills/Backend.tsx
+++ src/components/ui/Skills/Backend.tsx
@@ -2,6 +2,17 @@ export const Backend = () => {
   return (
     <>
       <p>this is Back end</p>
+      <div className="flex flex-wrap">
+        <img src="/back-skillset/apollo.svg" />
+        <img src="/back-skillset/express-js.svg" />
+        <img src="/back-skillset/GraphQL.svg" />
+        <img src="/back-skillset/hono.svg" />
+        <img src="/back-skillset/mysql.svg" />
+        <img src="/back-skillset/node-js.svg" />
+        <img src="/back-skillset/postgresql.svg" />
+        <img src="/back-skillset/postman.svg" className="w-12 h-12" />
+        <img src="/back-skillset/prisma.svg" />
+      </div>
     </>
   );
 };
