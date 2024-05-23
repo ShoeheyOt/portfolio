4964645e399fb342f5da267801429ec4b9e071ac
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-22 20:25:40 -0700

    add margin-x

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { Nav } from "./Nav";
+import { Nav } from "./components/Nav";
 import { Contact } from "./components/Contact";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 function App() {
   return (
-    <div className="font-mono font-semibold text-primary text-opacity-90">
+    <div className="font-mono font-semibold text-primary text-opacity-90 mx-28">
       <Nav />
       <Header />
       <Main />
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import { Skill } from "./Skill";
 
 export const Main = () => {
   return (
-    <div className="h-screen w-screen border border-white">
+    <div className="h-screen w-[calc(100vw-20rem)] border border-white">
       <div className="flex justify-between border border-white h-1/2">
         <About />
         <Skill />
