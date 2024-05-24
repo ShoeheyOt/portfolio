aaac3e00cc1ef04061fb12d602bf41b4c51eefce
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-24 15:54:46 -0700

    add url link

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -1,13 +1,24 @@
 export const ContactLink = () => {
+  const githubLink = import.meta.env.VITE_GITHUB_URL;
+  const linkedInLink = import.meta.env.VITE_LINKEDIN_URL;
+
   return (
     <div className="flex gap-20">
-      <a className="cursor-pointer hover:opacity-70">
+      <a
+        title="github"
+        className="cursor-pointer hover:opacity-70"
+        href={`${githubLink}`}
+      >
         <img src="/external/github.svg" className="w-12 h-12" />
       </a>
-      <a className="cursor-pointer hover:opacity-70">
+      <a
+        title="LinkedIn"
+        className="cursor-pointer hover:opacity-70"
+        href={`${linkedInLink}`}
+      >
         <img src="/external/linkedin.svg" />
       </a>
-      <a className="cursor-pointer hover:opacity-70">
+      <a title="Resume" className="cursor-pointer hover:opacity-70">
         <img src="/external/document.svg" />
       </a>
     </div>
