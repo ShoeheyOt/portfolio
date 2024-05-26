32416eceb2c868a586bd46c39a81fc168034ec5c
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-26 15:31:40 -0700

    implement:header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@ import { TypewriterHeader } from "./TypewriterHeader";
 
 export const Header = () => {
   return (
-    <div id="header" className="h-screen w-[calc(100vw-14rem)]">
+    <div
+      id="header"
+      className="h-screen w-screen lg:w-[calc(100vw-14rem)] mt-8 mb-8"
+    >
       <TypewriterHeader />
     </div>
   );
--- src/components/TypewriterHeader.tsx
+++ src/components/TypewriterHeader.tsx
@@ -14,7 +14,7 @@ export function TypewriterHeader() {
     },
     {
       text: "Shu ",
-      className: "dark:text-secondary underline decoration-secondary",
+      className: "text-2xl dark:text-secondary underline decoration-secondary",
     },
     {
       text: "full",
@@ -27,7 +27,7 @@ export function TypewriterHeader() {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem] mt-8">
+    <div className="flex flex-col items-center justify-center h-[40rem]">
       <TypewriterEffect words={words} />
       <p className="text-dark dark:text-themeWhite sm:text-lg md:text-2xl lg:text-4xl mt-10">
         Enthusiast who love and enjoy{" "}
