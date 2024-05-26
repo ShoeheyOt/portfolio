5fc7578a43e7fe3e32f48cc93a3400414a374c16
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-25 20:55:38 -0700

    add border to each card

--- src/components/ProjectContainer.tsx
+++ src/components/ProjectContainer.tsx
@@ -5,7 +5,7 @@ export const ProjectContainer = () => {
   return (
     <>
       <p className="text-3xl text-themeWhite -mt-8">My Projects</p>
-      <div className="flex justify-between w-full h-full">
+      <div className="flex gap-4 w-full h-full">
         {projectData.map((project) => (
           <ProjectCard key={project.dataId} project={project} />
         ))}
