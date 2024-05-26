687187df9bc1260c54a06dd84f68b4cafd78ee9a
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-25 21:11:22 -0700

    implement

--- src/components/ui/Skills/Frontend.tsx
+++ src/components/ui/Skills/Frontend.tsx
@@ -1,20 +1,23 @@
 export const Frontend = () => {
   return (
     <div className="flex flex-wrap gap-1">
-      <img src="/front-skillset/Astro.svg" />
-      <img src="/front-skillset/bootstrap.svg" />
-      <img src="/front-skillset/css.svg" />
-      <img src="/front-skillset/html.svg" />
-      <img src="/front-skillset/javascript.svg" />
-      <img src="/front-skillset/jQuery.svg" />
-      <img src="/front-skillset/nextjs.svg" />
-      <img src="/front-skillset/react.svg" />
-      <img src="/front-skillset/redux.svg" />
-      <img src="/front-skillset/shadcn.svg" />
-      <img src="/front-skillset/styled-components.svg" />
-      <img src="/front-skillset/tailwind-css.svg" />
-      <img src="/front-skillset/typescript.svg" />
-      <img src="/front-skillset/zod.svg" />
+      <img src="/front-skillset/Astro.svg" title="Astro" />
+      <img src="/front-skillset/bootstrap.svg" title="bootstrap" />
+      <img src="/front-skillset/css.svg" title="css" />
+      <img src="/front-skillset/html.svg" title="html" />
+      <img src="/front-skillset/javascript.svg" title="javascript" />
+      <img src="/front-skillset/jQuery.svg" title="jQuery" />
+      <img src="/front-skillset/nextjs.svg" title="nextjs" />
+      <img src="/front-skillset/react.svg" title="react" />
+      <img src="/front-skillset/redux.svg" title="redux" />
+      <img src="/front-skillset/shadcn.svg" title="shadcn" />
+      <img
+        src="/front-skillset/styled-components.svg"
+        title="styled-component"
+      />
+      <img src="/front-skillset/tailwind-css.svg" title="tailwindCSS" />
+      <img src="/front-skillset/typescript.svg" title="typescript" />
+      <img src="/front-skillset/zod.svg" title="zod" />
     </div>
   );
 };
