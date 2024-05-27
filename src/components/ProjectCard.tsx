cc64b45ea05aee61ff8d94ffbd2fbc16e3232f94
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-26 21:01:59 -0700

    adjust responsive when hover

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,7 @@ import { Project } from "../assets/data";
 
 export const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="w-1/3 2xl:w-1/4 relative group">
+    <div className="w-1/3 2xl:w-1/5 relative group">
       <div className="h-full w-full flex justify-center group-hover:border-2 group-hover:border-secondary group-hover:rounded-md">
         <img
           src={project.img}
@@ -10,8 +10,8 @@ export const ProjectCard = ({ project }: { project: Project }) => {
           alt={project.name}
         />
       </div>
-      <div className="absolute h-full w-full transition ease-linear top-4 left-2 2xl:top-24 2xl:left-10 opacity-0 group-hover:opacity-90">
-        <div className="text-lg 2xl:text-2xl h-1/4 text-themeWhite">
+      <div className="absolute h-full w-full transition ease-linear top-4 left-2 md:top-12 md:left-4 lg:top-16 lg:left-6 xl:top-20 xl:left-8 2xl:left-10 opacity-0 group-hover:opacity-90">
+        <div className="text-lg md:text-xl lg:text-2xl h-1/4 text-themeWhite">
           {project.name}
         </div>
         <ul className="flex gap-2 flex-wrap">
--- src/components/ProjectContainer.tsx
+++ src/components/ProjectContainer.tsx
@@ -7,7 +7,7 @@ export const ProjectContainer = () => {
       <p className="text-center text-3xl text-themeWhite mt-16 2xl:-mt-8 mb-8">
         My Projects
       </p>
-      <div className="flex flex-row gap-4 justify-center flex-wrap xl:flex-nowrap w-full 2xl:h-full">
+      <div className="flex flex-row gap-4 2xl:gap-0 justify-center flex-wrap xl:flex-nowrap 2xl:h-full">
         {projectData.map((project) => (
           <ProjectCard key={project.dataId} project={project} />
         ))}
