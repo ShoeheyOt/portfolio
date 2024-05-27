edbd1585c02f60283cb33de107b7e71ca246dee4
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-27 10:51:51 -0700

    adjust styling for about part

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import { Skill } from "./Skill";
 
 export const Main = () => {
   return (
-    <div className="2xl:h-screen 3xl:w-[calc(100vw-14rem)]">
+    <div className="2xl:h-screen">
       <div className="flex flex-col lg:flex-row justify-between lg:h-1/2 lg:px-12">
         <About />
         <Skill />
