8e33dcff6933f55abdbbfdbabd15bdfb2578a517
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-26 20:37:52 -0700

    feat/implement project part

--- src/components/ProjectContainer.tsx
+++ src/components/ProjectContainer.tsx
@@ -4,8 +4,10 @@ import { ProjectCard } from "./ProjectCard";
 export const ProjectContainer = () => {
   return (
     <>
-      <p className="text-3xl text-themeWhite -mt-8">My Projects</p>
-      <div className="flex flex-col 2xl:flex-row gap-4 w-full 2xl:h-full">
+      <p className="text-center text-3xl text-themeWhite mt-16 2xl:-mt-8 mb-8">
+        My Projects
+      </p>
+      <div className="flex flex-row gap-4 justify-center flex-wrap xl:flex-nowrap w-full 2xl:h-full">
         {projectData.map((project) => (
           <ProjectCard key={project.dataId} project={project} />
         ))}
