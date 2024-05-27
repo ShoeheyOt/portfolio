6a37220d4e800ab643977d1836daffbdfd215438
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-27 09:58:54 -0700

    add style explicitly to header

--- src/components/TypewriterHeader.tsx
+++ src/components/TypewriterHeader.tsx
@@ -5,9 +5,11 @@ export function TypewriterHeader() {
   const words = [
     {
       text: "Hi,",
+      className: "text-themeWhite",
     },
     {
       text: "I'm",
+      className: "text-themeWhite",
     },
     {
       text: "Shu",
@@ -16,18 +18,21 @@ export function TypewriterHeader() {
     },
     {
       text: "full",
+      className: "text-themeWhite",
     },
     {
       text: "stack",
+      className: "text-themeWhite",
     },
     {
       text: "developer.",
+      className: "text-themeWhite",
     },
   ];
   return (
     <div className="flex flex-col items-center justify-center h-[40rem]">
       <TypewriterEffect words={words} />
-      <p className="text-dark dark:text-themeWhite md:text-2xl xl:text-3xl mt-10">
+      <p className="text-themeWhite md:text-2xl xl:text-3xl mt-10">
         Enthusiast who love and enjoy{" "}
         <span className="text-secondary underline decoration-secondary">
           Coding
