b7c58b6783ba1f33962d546937f733ee62a4cbb4
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-05-27 11:35:07 -0700

    adjust margin bottom in form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,13 +5,13 @@ export const Contact = () => {
   return (
     <div
       id="contact"
-      className="flex flex-col items-center gap-12 mt-20 2xl:mt-32"
+      className="flex flex-col items-center gap-12 mt-40 2xl:mt-48"
     >
       <h2 className="text-3xl text-themeWhite mt-16">Contact Form</h2>
       <p className="text-md text-themeWhite text-center mb-12 px-4">
         Want to contact me? Send me message via below form :)
       </p>
-      <div className="flex flex-col items-center gap-28 lg:gap-24 w-full mb-4 lg:mb-16">
+      <div className="flex flex-col items-center gap-28 lg:gap-24 w-full mb-4 lg:mb-16 3xl:mb-44">
         <ContactForm />
         <ContactLink />
       </div>
