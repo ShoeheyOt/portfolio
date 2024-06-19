6cac3e8f9caa293571775339dca25f8855c9852d
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-06-19 13:58:14 -0700

    fix the width in desktop view

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,7 @@ import { Project } from "../assets/data";
 
 export const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="card w-full flex flex-col items-center gap-4 sm:gap-2 lg:gap-4 py-6 px-3 lg:px-8 mx-14 lg:mx-28 2xl:mx-52 bg-themeWhite rounded-xl">
+    <div className="card w-full flex flex-col items-center gap-4 sm:gap-2 lg:gap-4 py-6 px-3 lg:px-8 mx-14 lg:mx-28 2xl:mx-52 3xl:mx-80 bg-themeWhite rounded-xl">
       <div className="text-dark w-5/6 md:w-2/3 xl:w-3/5 2xl:w-1/2">
         <h2 className="text-center text-2xl md:text-3xl lg:text-4xl">
           {project.name}
