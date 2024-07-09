6f8c7e858f93979cdb8c3e04961d83b45327be21
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-07-09 14:05:09 -0700

    update resume

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -31,7 +31,7 @@ export const ContactLink = () => {
       <a
         aria-label="jump to resume"
         title="Resume"
-        href="resume/resume.pdf"
+        href="resume/resume1.pdf"
         target="blank"
         className="cursor-pointer hover:opacity-70"
       >
