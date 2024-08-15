fa7d6ec70337a48761ded348a83d5a9c807e68ba
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-14 18:29:58 -0700

    change the logo in description of logo part

--- src/components/LogoDescription.tsx
+++ src/components/LogoDescription.tsx
@@ -4,7 +4,7 @@ export const LogoDescription = () => {
       <h2 className="text-center text-3xl mb-2">Secret of Logo</h2>
       <div className="grid grid-cols-3 grid-rows-3">
         <img
-          src="hero/logo1.svg"
+          src="hero/logo.svg"
           className="w-16 md:w-20 lg:w-48 2xl:w-60 h-auto col-start-1 row-start-1 lg:row-span-3 justify-self-center lg:justify-self-start self-center"
         />
         <p className="col-start-2 col-span-2 row-start-1 text-xs self-center md:text-sm lg:text-base xl:text-lg 3xl:text-2xl">
