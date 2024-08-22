22b5f19a7969f619f03b367accefbeb7bfc0f3ce
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-22 12:48:36 -0700

    update readability in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,23 @@
 export const About = () => {
   return (
-    <div id="about" className="flex flex-col gap-2 mb-6 lg:mb-12">
-      <p className="text-themeWhite text-3xl mb-2 text-center">About myself</p>
-      <p className="mb-4 text-themeWhite text-xs md:text-xl xl:text-2xl">
+    <div id="about" className="flex flex-col gap-2 mb-6 lg:mb-12 tracking-wide">
+      <p className="text-themeWhite text-5xl/10 mb-2 text-center">
+        About myself
+      </p>
+      <p className="mb-4 text-themeWhite text-xs/normal md:text-2xl/normal xl:text-4xl/normal">
         Thank you for finding me! <br />
         As an enthusiastic front end developer based in Vancouver, I am
-        passionate about developing user-centered application. I am currently
-        focusing on <span className="text-primary underline">Hono</span>,{" "}
-        <span className="text-primary underline">Rust</span> and{" "}
-        <span className="text-primary underline">Vim</span>.
+        passionate about developing user-centered application proficiently in{" "}
+        <span className="text-primary underline">Typescript</span>,{" "}
+        <span className="text-primary underline">React</span> and{" "}
+        <span className="text-primary underline">TailwindCSS</span>. I am
+        currently curious about{" "}
+        <span className="text-danger underline">Hono</span>,{" "}
+        <span className="text-danger underline">Rust</span> and{" "}
+        <span className="text-danger underline">Vim</span>.
       </p>
 
-      <p className="  text-themeWhite text-xs md:text-xl xl:text-2xl">
+      <p className="  text-themeWhite text-xs/normal md:text-2xl/normal xl:text-4xl/normal">
         When I am not coding, I enjoy hiking, playing basketball and drinking
         beer with my friends. I love exploring breweries, and so far, my
         favorite is Philips Brewing in Victoria.
