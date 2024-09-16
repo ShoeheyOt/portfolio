06696b18c93b2e6f3ab1b8112abf4356d62129cc
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-09-15 23:22:54 -0700

    refactor:update font size for responsive

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,13 +5,13 @@ export const Contact = () => {
   return (
     <div
       id="contact"
-      className="flex flex-col items-center gap-12 mt-40 2xl:mt-48 px-4 md:px-16 lg:px-40 3xl:px-64"
+      className="text-themeWhite flex flex-col items-center gap-12 mt-40 2xl:mt-48 px-4 md:px-16 lg:px-40 3xl:px-64"
     >
-      <h2 className="text-3xl mt-16 w-full px-4 text-center">Contact Form</h2>
-      <p className="text-md lg:text-lg text-center mb-12">
-        <span className="font-bold underline">Thank you</span> for visiting my
-        website. If you have any question about my project or about me, please
-        feel free to contact me using the form below or via LinkedIn.
+      <h2 className="text-3xl/10 xl:text-5xl/10 mt-16 w-full px-4 text-center">Contact Form</h2>
+      <p className="text-md md:text-2xl/normal xl:text-4xl/normal text-center mb-12">
+        <span className="text-primary font-bold underline">Thank you</span> for visiting my website.
+        If you have any question about my project or about me, please feel free to contact me using
+        the form below or via LinkedIn.
       </p>
       <div className="flex flex-col items-center gap-28 lg:gap-24 w-full mb-4 lg:mb-16 3xl:mb-44">
         <ContactForm />
--- src/components/LogoDescription.tsx
+++ src/components/LogoDescription.tsx
@@ -1,35 +1,30 @@
 export const LogoDescription = () => {
   return (
     <div className="text-themeWhite mb-6 lg:mb-12">
-      <h2 className="text-center text-3xl mb-2">Secret of Logo</h2>
+      <h2 className="text-center text-3xl/10 xl:text-5xl/10 mb-2 4xl:mb-0 4xl:mt-10">
+        Secret of Logo
+      </h2>
       <div className="grid grid-cols-3 grid-rows-3">
         <img
           src="hero/logo.svg"
-          className="w-16 md:w-20 lg:w-48 2xl:w-60 h-auto col-start-1 row-start-1 lg:row-span-3 justify-self-center lg:justify-self-start self-center"
+          className="w-16 md:w-20 lg:w-48 2xl:w-60 h-auto col-start-1 row-start-1 lg:row-start-2 justify-self-center lg:justify-self-start self-center"
         />
-        <p className="col-start-2 col-span-2 row-start-1 text-xs self-center md:text-sm lg:text-base xl:text-lg 3xl:text-2xl">
-          I created this logo to represent myself. Each shape and color has
-          meanings.
+        <p className="col-start-2 lg:col-start-1 col-span-2 lg:col-span-3 row-start-1 text-xs self-center md:text-sm  lg:text-2xl xl:text-3xl xl:text-center xl:-mx-4 xl:-mb-8 3xl:text-2xl 4xl:text-4xl">
+          I created this logo to represent myself. Each shape and color has meanings.
         </p>
-        <div className="col-span-3 lg:col-span-2  row-span-2 text-xs md:text-sm lg:text-base xl:text-lg 3xl:text-2xl">
+        <div className="col-span-3 lg:col-span-2 row-span-2 text-xs md:text-sm lg:text-base xl:text-2xl 3xl:text-2xl 4xl:text-3xl xl:-ml-32 4xl:-ml-44">
           <p>
-            <span className="font-bold">Shape</span>: My last name is Ota
-            (pronounce is similar to 'ought'). written as "大" and "田" in
-            Chinese characters. The logo layers these two characters.
+            <span className="font-bold">Shape</span>: My last name is Ota (pronounce is similar to
+            'ought'). written as "大" and "田" in Chinese characters. The logo layers these two
+            characters.
           </p>
           <br />
           <div>
             <span className="font-bold">Color</span>: <br />
             <ul className="list-disc list-inside">
-              <li>
-                Yellow: Represents joy and happiness, which I convey through my
-                words.
-              </li>
-              <li>
-                Blue: Represents strength and royalty, which I demonstrate
-                through my actions
-              </li>
-              <li>Red: Represents enthusiasm and passion within me</li>
+              <li>Yellow: Joy and happiness, which I convey through my words.</li>
+              <li>Blue: Strength and royalty, which I demonstrate through my actions</li>
+              <li>Red: Enthusiasm and passion within me</li>
             </ul>
           </div>
         </div>
